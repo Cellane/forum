e3dd5dedd182e5ba267bc906208877b31f053d30
Milan Vit <milanvit@milanvit.net>
2019-08-30 18:06:36 +0900

    Episode 5 Complete

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -26,5 +26,31 @@
         @endforeach
       </div>
     </div>
+
+    @if(auth()->check())
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          <form action="{{ $thread->path() . '/replies' }}" method="post">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+              <textarea
+                name="body"
+                id="body"
+                rows="5"
+                class="form-control"
+                placeholder="Have something to say?"
+              ></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-default">Post</button>
+          </form>
+        </div>
+      </div>
+    @else
+      <p class="text-center">
+        Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.
+      </p>
+    @endif
   </div>
 @endsection
