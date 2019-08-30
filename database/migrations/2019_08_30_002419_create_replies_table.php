914508133410c8326c8a07036fd6c481cd5324d9
Milan Vit <milanvit@milanvit.net>
2019-08-30 11:55:16 +0900

    Episode 1 Complete

---
+++ database/migrations/2019_08_30_002419_create_replies_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('thread_id');
+            $table->unsignedInteger('user_id');
+            $table->text('body');
+            $table->timestamps();
+
+            $table->foreign('thread_id')
+                ->references('id')
+                ->on('threads')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
