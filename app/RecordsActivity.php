ee813f08827bb5b02532c3970a927a067e1970b7
Milan Vit <milanvit@milanvit.net>
2019-09-03 07:56:48 +0900

    Episode 28 Complete

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -15,6 +15,10 @@ trait RecordsActivity
                 $model->recordActivity($event);
             });
         }
+
+        static::deleting(function ($model) {
+            $model->activity()->delete();
+        });
     }
 
     protected static function getActivitiesToRecord()
--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Activity;
 use App\Reply;
 use App\Thread;
 use Tests\TestCase;
@@ -82,6 +83,7 @@ class CreateThreadsTest extends TestCase
 
         $this->assertDatabaseMissing('threads', ['id' => $thread->id]);
         $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
+        $this->assertEquals(0, Activity::count());
     }
 
     private function publishThread($overrides)
