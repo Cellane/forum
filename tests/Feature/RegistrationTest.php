166b175bc5929fb3c9cbfe74c9bf9096baaaf264
Milan Vit <milanvit@milanvit.net>
2019-09-10 09:32:39 +0900

    Episode 74 Complete

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -46,7 +46,11 @@ class RegistrationTest extends TestCase
 
         $this->get(route('register.confirm', ['token' => $user->confirmation_token]))
             ->assertRedirect(route('threads'));
-        $this->assertTrue($user->fresh()->confirmed);
+
+        tap($user->fresh(), function ($user) {
+            $this->assertTrue($user->confirmed);
+            $this->assertNull($user->confirmation_token);
+        });
     }
 
     /** @test */
