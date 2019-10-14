06f450aca2e6565aff23d128acf1fc7c1611a241
Milan Vit <milanvit@milanvit.net>
2019-10-14 23:11:24 +0900

    Update Purify acceptable tags

--- config/purify.php
+++ config/purify.php
@@ -65,7 +65,7 @@ return [
         |
         */
 
-        'HTML.Allowed' => 'h1,h2,h3,h4,h5,h6,b,strong,i,em,a[href|title],ul,ol,li,p[style],br,span,img[width|height|alt|src],pre',
+        'HTML.Allowed' => 'h1,h2,h3,h4,h5,h6,b,strong,i,em,a[href|title],ul,ol,li,p[style],br,span,img[width|height|alt|src],div,pre,code',
 
         /*
         |--------------------------------------------------------------------------
