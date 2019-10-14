1b15cdad657526226eadaac91fa70595edbb901e
Milan Vit <milanvit@milanvit.net>
2019-10-14 23:16:18 +0900

    Require e-mail confirmation for new thread page

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -14,7 +14,7 @@ class ThreadController extends Controller
     public function __construct()
     {
         $this->middleware('auth')->except(['index', 'show']);
-        $this->middleware('must-be-confirmed')->only('store');
+        $this->middleware('must-be-confirmed')->only(['create', 'store']);
     }
 
     /**
